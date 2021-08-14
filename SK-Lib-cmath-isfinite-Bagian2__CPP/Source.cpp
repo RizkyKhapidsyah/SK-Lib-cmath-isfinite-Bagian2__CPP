3cd42c58f2a38611ea0dab8d287c9afefa78bfbe
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-14 14:57:35 +0700

    Add project files.

---
+++ SK-Lib-cmath-isfinite-Bagian2__CPP/Source.cpp
@@ -0,0 +1,22 @@
+
+#include <conio.h>
+#include <iostream>
+#include <cmath>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	float x = 19.0;
+
+	cout << "Nilai dari x = " << x << endl;
+	cout << "isfinite(x) = " << isfinite(x);
+
+	_getch();
+	return 0;
+}
